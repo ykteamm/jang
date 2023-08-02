cf7c9f8dd96e8cb1e2c82f263d9c8dcbe6bae966
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2023-08-02 09:37:23 +0500

    'karma-update'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -464,7 +464,94 @@ class HomeController extends Controller
     public function index()
     {
 
-        
+        $teams = Team::with('team_members')->get();
+
+            $dt = date('2023-08-01');
+            $tashqi = 0;
+            $jamoaviy = 0;
+            foreach ($teams as  $team) {
+                foreach ($team->team_members as $key => $value) {
+                
+                    $distiplina = Shift::where('user_id',$value->user_id)->whereDate('open_date',$dt)->exists();
+                    if($distiplina)
+                    {
+                        $new = TeamBattleKarma::create([
+                            'team_id' => $team->id,
+                            'user_id' => $value->user_id,
+                            'karma' => 1,
+                            'comment' => 'Distiplina uchun',
+                            'active' => 1,
+                        ]);
+                    }else{
+                        $new = TeamBattleKarma::create([
+                            'team_id' => $team->id,
+                            'user_id' => $value->user_id,
+                            'karma' => -1,
+                            'comment' => 'Ishga chiqmaganligi uchun',
+                            'active' => 1,
+                        ]);
+                    }
+                    if(date('w') == 0 && $distiplina)
+                    {
+                        $prodaja = DB::table('tg_productssold as p')
+                        ->selectRaw('COALESCE(SUM(p.number * p.price_product),0) as prodaja')
+                        ->where('p.user_id', $value->user_id)
+                        ->whereDate('p.created_at', $dt)
+                        ->value('prodaja');
+                        
+                        if($prodaja >= 500000)
+                        {
+                            $new = TeamBattleKarma::create([
+                                        'team_id' => $team->id,
+                                        'user_id' => $value->user_id,
+                                        'karma' => 2,
+                                        'comment' => 'Yakshanba kuni savdoni 500 mingdan oshirganligi uchun',
+                                        'active' => 1,
+                                    ]);
+                        }
+                    }
+
+                    $prodaja = DB::table('tg_productssold as p')
+                        ->selectRaw('COALESCE(SUM(p.number * p.price_product),0) as prodaja')
+                        ->where('p.user_id', $value->user_id)
+                        ->whereDate('p.created_at', $dt)
+                        ->value('prodaja');
+                    
+                    $jamoaviy += $prodaja;
+
+                
+
+                    $prodajatashqi = DB::table('tg_productssold as p')
+                        ->selectRaw('COALESCE(SUM(p.number * p.price_product),0) as prodaja')
+                        ->where('p.user_id', $value->user_id)
+                        ->where('p.pharm_id', 42)
+                        ->whereDate('p.created_at', $dt)
+                        ->value('prodaja');
+                    
+                    $tashqi += $prodajatashqi;
+                
+                }
+            }
+
+            if($jamoaviy >= 5000000)
+                {
+                    $new = TeamBattleKarma::create([
+                        'team_id' => $team->id,
+                        'karma' => 10,
+                        'comment' => 'Jamoa 1kunda 5 mlndan oshirganligi uchun',
+                        'active' => 1,
+                    ]);
+                }
+            
+            if($tashqi >= 3000000)
+            {
+                $new = TeamBattleKarma::create([
+                    'team_id' => $team->id,
+                    'karma' => 20,
+                    'comment' => 'Jamoa 1kunda tashqi savdoda 3 mlndan oshirganligi uchun',
+                    'active' => 1,
+                ]);
+            }
 
         if(userme()->rm == 1)
         {
