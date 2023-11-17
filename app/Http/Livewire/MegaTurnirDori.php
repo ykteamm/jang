4fd37ca14968ccce17414602b3246664fbc9183b
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2023-11-17 18:10:57 +0500

    dat

--- app/Http/Livewire/MegaTurnirDori.php
+++ app/Http/Livewire/MegaTurnirDori.php
@@ -75,8 +75,8 @@ class MegaTurnirDori extends Component
                 $name = $value->first_name.' '.substr($value->last_name,0,1);
 
                 $sold1 = AllSold::where('user_id',$value->id)
-                ->whereDate('created_at','>=','2023-11-10')
-                ->whereDate('created_at','<=','2023-11-16')
+                ->whereDate('created_at','>=','2023-11-17')
+                ->whereDate('created_at','<=','2023-11-23')
                     ->where('medicine_id',29)
                     ->sum('number');
     
