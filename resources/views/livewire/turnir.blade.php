ecbbdef2ac7de3f7e6d7c6465fff0229f4f337ee
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2023-12-29 10:26:29 +0500

    fd

--- resources/views/livewire/turnir.blade.php
+++ resources/views/livewire/turnir.blade.php
@@ -571,8 +571,8 @@
     </div>
     @endif
     <script>
-        var dday = <?php echo json_encode(date('d', strtotime('2023-12-28'))); ?>;
-        var dname = <?php echo json_encode(date('F', strtotime('2023-12-28'))); ?>;
+        var dday = <?php echo json_encode(date('d', strtotime('2023-01-04'))); ?>;
+        var dname = <?php echo json_encode(date('F', strtotime('2023-01-04'))); ?>;
         var countDownDate = new Date(dname + " " + dday + ", 2023 23:59:59").getTime();
 
         var x = setInterval(function() {
