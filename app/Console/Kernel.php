c4d68bebf50cffdf59811cf76755ebab260f385d
ergashevtolqin0103 <ergashevtolqin0103@gmail.com>
2024-02-01 11:49:19 +0500

    'kkk'

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('battle:elchi')
-        ->dailyAt('11:49');
+        ->dailyAt('11:52');
 
         $schedule->command('battleEnd:elchi')
         ->dailyAt('23:55');
