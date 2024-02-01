989a0726eba5d67f685d0b126e48120eb6eff536
ergashevtolqin0103 <ergashevtolqin0103@gmail.com>
2024-02-01 11:44:27 +0500

    'kkk'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -148,10 +148,15 @@ class HomeController extends Controller
 
         $battle_yes = 'yes';
         
-        // if($my_id == 5)
-        // {
-        //     return $my_battle;
-        // }
+        if($my_id == 5)
+        {
+            $my_battle = UserBattle::with('u1ids','u2ids')
+            ->whereDate('start_day','<=',$battle_date)
+            ->whereDate('end_day','>=',$battle_date)
+            ->get();
+
+            return $my_battle;
+        }
         
         if(count($my_battle) == 0)
         {
