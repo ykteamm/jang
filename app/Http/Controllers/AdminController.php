0630a301d621009f2579ad32d2ba62474acebb8e
Abrorbek Toshpo'latov <109383863+Abrorbek19@users.noreply.github.com>
2024-03-14 12:23:19 +0500

    star

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -67,28 +67,20 @@ class AdminController extends Controller
 
     public function TTL()
     {
-        $ws = DB::table('tg_user')->where('id',79)->first();
 
-        $work_start = '2024-01-01';
-        $date_end = '2024-01-31';
-        $plus_date = date('Y-m-d',(strtotime ( '30 days' , strtotime ( $work_start ) ) ));
+        $stars = TopshiriqStar::select(
+            'topshiriq_star.tg_user_id',
+            'tg_user.first_name',
+            'tg_user.last_name',
+            DB::raw('SUM(topshiriq_star.star) as total_star')
+        )
+            ->join('tg_user', 'tg_user.id', '=', 'topshiriq_star.tg_user_id')
+            ->groupBy('topshiriq_star.tg_user_id',  'tg_user.first_name', 'tg_user.last_name')
+            ->orderBy('total_star','desc')
+            ->get();
 
-//        return $plus_date;
+        return $stars;
 
-//        $work_day = Shift::where('user_id',79)
-//            ->where('open_date','!=',NULL)
-//            ->whereDate('open_date','>=',$work_start)
-//            ->whereDate('open_date','<=',$date_end)
-//            ->count('id');
-//
-//        return $work_day;
-        $work_day = Shift::where('user_id', 79)
-            ->where('open_date', '!=', NULL)
-            ->whereDate('open_date', '>=', $work_start)
-            ->whereDate('open_date', '<=', $date_end)
-            ->count();
-
-        return $work_day;
     }
 
 }
